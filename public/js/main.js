78939242649804160b45abeaa3bf610ed5d5a0e2
Sergi Heras <sergihese@hotmail.com>
2019-03-30 11:46:27 +0100

    addTimestampsToJavascriptTags

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,16 @@ function sortMe(tableId) {
     })
     .appendTo(tbody);
 }
+
+function addTimestampsToScriptTags() {
+  const date = Date.now().toString();
+  const randomNumber = Math.floor(Math.random() * 999999).toString();
+  let scripts = $("script");
+  console.log("scripts");
+  console.log(scripts);
+
+  $(scripts).each(function() {
+    if ($(this).attr("src"))
+      $(this).attr("src", $(this).attr("src") + "?" + date + randomNumber);
+  });
+}
