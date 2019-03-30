9defa8d84835199c434b2b0dea88db0b50bfee87
Sergi Heras <sergihese@hotmail.com>
2019-03-30 11:48:32 +0100

    remove consoleLogs in addTimestampsToScripTags

--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,6 @@ function addTimestampsToScriptTags() {
   const date = Date.now().toString();
   const randomNumber = Math.floor(Math.random() * 999999).toString();
   let scripts = $("script");
-  console.log("scripts");
-  console.log(scripts);
-
   $(scripts).each(function() {
     if ($(this).attr("src"))
       $(this).attr("src", $(this).attr("src") + "?" + date + randomNumber);
