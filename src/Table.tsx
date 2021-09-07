380334ae6a34ae37a1f785304e593b161bf13cf5
Sergi <Sergih28@users.noreply.github.com>
2021-09-07 13:10:12 +0200

    Simplified Table code placing out of the component the main parts

--- src/Table.tsx
+++ src/Table.tsx
@@ -2,6 +2,77 @@ import { tabs } from './Navbar'
 import { useSetup, SetupCompleteProps } from './SetupContext'
 import { SetupProps } from './setup'
 import { Tab } from '@headlessui/react'
+import { ReactElement } from 'react'
+
+const Tabs = ({ tabs }: { tabs: string[] }): ReactElement => (
+  <Tab.List>
+    {tabs.map((tab: string, key: number) => (
+      <Tab key={key}>{tab}</Tab>
+    ))}
+  </Tab.List>
+)
+
+const Panels = ({
+  tabs,
+  setups,
+}: {
+  tabs: string[]
+  setups: SetupCompleteProps[] | undefined
+}) => (
+  <Tab.Panels>
+    {tabs.map((tab: string, key: number) => (
+      <Tab.Panel key={key}>
+        <table>
+          <thead>
+            <tr>
+              {setups?.map((setup: SetupCompleteProps, key2: number) => (
+                <th key={key2}>{setup.name}</th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            <>
+              {setups &&
+                setups.length > 0 &&
+                setups[0].content.map((content: SetupProps, key3: number) => (
+                  <>
+                    {content.tab === tab && (
+                      <tr key={key3}>
+                        {setups?.map(
+                          (setup_inside: SetupCompleteProps, key4: number) => (
+                            <>
+                              {content.key ===
+                                setup_inside.content.find(
+                                  (cont: SetupProps) => cont.key === content.key
+                                )?.key && (
+                                <td key={key4}>
+                                  {setup_inside.content.find(
+                                    (cont: SetupProps) =>
+                                      cont.key === content.key
+                                  )?.tab === tab &&
+                                    `${content.name}: ${
+                                      setup_inside.content.find(
+                                        (cont: SetupProps) =>
+                                          cont.key === content.key
+                                      )?.value
+                                    }`}
+                                </td>
+                              )}
+                            </>
+                          )
+                        )}
+                      </tr>
+                    )}
+                  </>
+                ))}
+            </>
+          </tbody>
+        </table>
+      </Tab.Panel>
+    ))}
+  </Tab.Panels>
+)
 
 const Table = () => {
   const { setups } = useSetup()
@@ -10,70 +81,8 @@ const Table = () => {
     <>
       <div className="w-full max-w-md px-2 py-16 sm:px-0">
         <Tab.Group>
-          <Tab.List>
-            {tabs.map((tab: string, key: number) => (
-              <Tab key={key}>{tab}</Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels>
-            {tabs.map((tab: string, key: number) => (
-              <Tab.Panel>
-                <table>
-                  <thead>
-                    <tr>
-                      {setups?.map((setup: SetupCompleteProps, key: number) => (
-                        <th key={key}>{setup.name}</th>
-                      ))}
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    <>
-                      {setups &&
-                        setups.length > 0 &&
-                        setups[0].content.map(
-                          (content: SetupProps, key3: number) => (
-                            <>
-                              {content.tab === tab && (
-                                <tr key={key3}>
-                                  {setups?.map(
-                                    (
-                                      setup_inside: SetupCompleteProps,
-                                      key4: number
-                                    ) => (
-                                      <>
-                                        {content.key ===
-                                          setup_inside.content.find(
-                                            (cont: SetupProps) =>
-                                              cont.key === content.key
-                                          )?.key && (
-                                          <td key={key4}>
-                                            {setup_inside.content.find(
-                                              (cont: SetupProps) =>
-                                                cont.key === content.key
-                                            )?.tab === tab &&
-                                              `${content.name}: ${
-                                                setup_inside.content.find(
-                                                  (cont: SetupProps) =>
-                                                    cont.key === content.key
-                                                )?.value
-                                              }`}
-                                          </td>
-                                        )}
-                                      </>
-                                    )
-                                  )}
-                                </tr>
-                              )}
-                            </>
-                          )
-                        )}
-                    </>
-                  </tbody>
-                </table>
-              </Tab.Panel>
-            ))}
-          </Tab.Panels>
+          <Tabs tabs={tabs} />
+          <Panels tabs={tabs} setups={setups} />
         </Tab.Group>
       </div>
     </>
