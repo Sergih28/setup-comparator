6f333e853fc9bd608418bc4c4b1fddc473fcffb4
Sergi <Sergih28@users.noreply.github.com>
2021-09-07 13:00:17 +0200

    Add tailwindcss
    - Remove old js files
    - Remove old scripts/links/imports in html
    - Showing setups in a table, without styling yet

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from './Footer'
 import Main from './Main'
 import Navbar from './Navbar'
 import { SetupProvider } from './SetupContext'
+import './App.css'
 
 function App() {
   return (
--- src/Table.tsx
+++ src/Table.tsx
@@ -1,32 +1,81 @@
 import { tabs } from './Navbar'
-import { useSetup } from './SetupContext'
+import { useSetup, SetupCompleteProps } from './SetupContext'
+import { SetupProps } from './setup'
+import { Tab } from '@headlessui/react'
 
 const Table = () => {
   const { setups } = useSetup()
 
   return (
     <>
-      {tabs.map((tab: string, key: number) => (
-        <div
-          key={key}
-          id={`${tab}tab`}
-          custom-role="table"
-          className="col s12 customTab no-padding"
-        >
-          {/*<table id={`${tab}tabTable`} className="highlight responsive-table">
-            <thead>
-              <tr>
-                <th></th>
-                {setups?.map((setup: SetupCompleteProps, key2: number) => (
-                  <th key={key2}>{setup?.name}</th>
-                ))}
-              </tr>
-            </thead>
-          </table>*/}
-        </div>
-      ))}
-      <p>NAME: {setups?.map((setup) => setup?.name)}</p>
-      <p>{JSON.stringify(setups)}</p>
+      <div className="w-full max-w-md px-2 py-16 sm:px-0">
+        <Tab.Group>
+          <Tab.List>
+            {tabs.map((tab: string, key: number) => (
+              <Tab key={key}>{tab}</Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels>
+            {tabs.map((tab: string, key: number) => (
+              <Tab.Panel>
+                <table>
+                  <thead>
+                    <tr>
+                      {setups?.map((setup: SetupCompleteProps, key: number) => (
+                        <th key={key}>{setup.name}</th>
+                      ))}
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <>
+                      {setups &&
+                        setups.length > 0 &&
+                        setups[0].content.map(
+                          (content: SetupProps, key3: number) => (
+                            <>
+                              {content.tab === tab && (
+                                <tr key={key3}>
+                                  {setups?.map(
+                                    (
+                                      setup_inside: SetupCompleteProps,
+                                      key4: number
+                                    ) => (
+                                      <>
+                                        {content.key ===
+                                          setup_inside.content.find(
+                                            (cont: SetupProps) =>
+                                              cont.key === content.key
+                                          )?.key && (
+                                          <td key={key4}>
+                                            {setup_inside.content.find(
+                                              (cont: SetupProps) =>
+                                                cont.key === content.key
+                                            )?.tab === tab &&
+                                              `${content.name}: ${
+                                                setup_inside.content.find(
+                                                  (cont: SetupProps) =>
+                                                    cont.key === content.key
+                                                )?.value
+                                              }`}
+                                          </td>
+                                        )}
+                                      </>
+                                    )
+                                  )}
+                                </tr>
+                              )}
+                            </>
+                          )
+                        )}
+                    </>
+                  </tbody>
+                </table>
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
     </>
   )
 }
