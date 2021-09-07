7e8e94cd8af695bfb07642dd7632dcb2ce209054
Sergi <Sergih28@users.noreply.github.com>
2021-09-07 13:33:59 +0200

    Simplify Table code
    - Move outside the main function TableHead and TableBody

--- src/Table.tsx
+++ src/Table.tsx
@@ -16,6 +16,59 @@ const Tabs = ({ tabs }: TabsProps): ReactElement => (
   </Tab.List>
 )
 
+interface TableHeadProps {
+  setups: SetupCompleteProps[] | undefined
+}
+
+const TableHead = ({ setups }: TableHeadProps) => (
+  <thead>
+    <tr>
+      {setups?.map((setup: SetupCompleteProps, key2: number) => (
+        <th key={key2}>{setup.name}</th>
+      ))}
+    </tr>
+  </thead>
+)
+
+interface TableBodyProps {
+  setups: SetupCompleteProps[] | undefined
+  tab: string
+}
+
+const TableBody = ({ setups, tab }: TableBodyProps) => (
+  <tbody>
+    {setups &&
+      setups.length > 0 &&
+      setups[0].content.map((content: SetupProps, key3: number) => (
+        <>
+          {content.tab === tab && (
+            <tr key={key3}>
+              {setups?.map((setup_inside: SetupCompleteProps, key4: number) => (
+                <>
+                  {content.key ===
+                    setup_inside.content.find(
+                      (cont: SetupProps) => cont.key === content.key
+                    )?.key && (
+                    <td key={key4}>
+                      {setup_inside.content.find(
+                        (cont: SetupProps) => cont.key === content.key
+                      )?.tab === tab &&
+                        `${content.name}: ${
+                          setup_inside.content.find(
+                            (cont: SetupProps) => cont.key === content.key
+                          )?.value
+                        }`}
+                    </td>
+                  )}
+                </>
+              ))}
+            </tr>
+          )}
+        </>
+      ))}
+  </tbody>
+)
+
 interface PanelsProps {
   tabs: string[]
   setups: SetupCompleteProps[] | undefined
@@ -26,51 +79,8 @@ const Panels = ({ tabs, setups }: PanelsProps) => (
     {tabs.map((tab: string, key: number) => (
       <Tab.Panel key={key}>
         <table>
-          <thead>
-            <tr>
-              {setups?.map((setup: SetupCompleteProps, key2: number) => (
-                <th key={key2}>{setup.name}</th>
-              ))}
-            </tr>
-          </thead>
-
-          <tbody>
-            <>
-              {setups &&
-                setups.length > 0 &&
-                setups[0].content.map((content: SetupProps, key3: number) => (
-                  <>
-                    {content.tab === tab && (
-                      <tr key={key3}>
-                        {setups?.map(
-                          (setup_inside: SetupCompleteProps, key4: number) => (
-                            <>
-                              {content.key ===
-                                setup_inside.content.find(
-                                  (cont: SetupProps) => cont.key === content.key
-                                )?.key && (
-                                <td key={key4}>
-                                  {setup_inside.content.find(
-                                    (cont: SetupProps) =>
-                                      cont.key === content.key
-                                  )?.tab === tab &&
-                                    `${content.name}: ${
-                                      setup_inside.content.find(
-                                        (cont: SetupProps) =>
-                                          cont.key === content.key
-                                      )?.value
-                                    }`}
-                                </td>
-                              )}
-                            </>
-                          )
-                        )}
-                      </tr>
-                    )}
-                  </>
-                ))}
-            </>
-          </tbody>
+          <TableHead setups={setups} />
+          <TableBody setups={setups} tab={tab} />
         </table>
       </Tab.Panel>
     ))}
