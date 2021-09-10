42da456adeedf2f9169e99772fe71d121f4fb6b3
Sergi <Sergih28@users.noreply.github.com>
2021-09-10 11:35:12 +0200

    Add chakra UI (#16)
    Add Chakra UI
    
    - Make UI totally responsive, using css grid
    - Remove craco, tailwindcss and headlessUI
    - Replace css for scss
    - Using rfactor 2 main colors for the footer and header background
    - Using Chakra UI Icons
    - Using react-icons
    - Add author in package.json and Footer
    - Add transitions for Navbar, Footer and Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import Footer from './Footer'
 import Main from './Main'
 import Navbar from './Navbar'
 import { SetupProvider } from './SetupContext'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <SetupProvider>
-        <Navbar />
-        <Main />
-        <Footer />
-      </SetupProvider>
+      <ChakraProvider>
+        <SetupProvider>
+          <Navbar />
+          <Main />
+          <Footer />
+        </SetupProvider>
+      </ChakraProvider>
     </div>
   )
 }
