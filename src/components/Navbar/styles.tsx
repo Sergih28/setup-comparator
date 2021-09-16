123b3a1d4392d1c7bc0826ef1079e6cc28970667
Sergi <Sergih28@users.noreply.github.com>
2021-09-16 14:49:30 +0200

    Lint the same way inside styled components files

---
+++ src/components/Navbar/styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  background: red;
+  border: 1px solid red;
+  color: black;
+  color: #ffffff;
+  display: block;
+  font-size: 3rem;
+  height: 40px;
+  position: absolute;
+  top: 0;
+  width: 400px;
+`
+
+const x = ''
