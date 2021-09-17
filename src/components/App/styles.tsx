c42df51ddff96aea22160b803553578ec1fba858
Sergi <Sergih28@users.noreply.github.com>
2021-09-17 02:21:50 +0200

    Update html and body styles

--- src/components/App/styles.tsx
+++ src/components/App/styles.tsx
@@ -1,13 +1,22 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  html {
+    width: 100%;
+    height: 100%;
+    display: table;
+}
+
+  body {
+    width: 100%;
+    display: table-cell;
+}
+
   html, body {
     font-family: sans-serif;
-    height: 100%;
-    width: 100%;
     margin: 0;
     padding: 0;
-  }
+}
 `
 
 export const Wrapper = styled.div`
